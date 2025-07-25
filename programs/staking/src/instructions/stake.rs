5036742a43964f045926e5e923dd572adb63f34b
yash vikram <yashvikram8250@gmail.com>
2025-07-25 15:06:20 +0530

    stake.rs

--- programs/staking/src/instructions/stake.rs
+++ programs/staking/src/instructions/stake.rs
@@ -13,21 +13,36 @@ use anchor_spl::{
 };
 use crate::{ state::{ StakeAccount, StakeConfig, UserAccount }, user_account, error::StakeError };
 
+/*
+   - this instruction will allow the user to stake nfts.
+   - accounts:
+        - user
+        - mint
+        - collection mint
+        - metadata
+        - edition
+        - config
+        - user_account
+        - stake_account
+        - user_mint_ata
+        - the three programs
+*/
+
 #[derive(Accounts)]
 pub struct Stake<'info> {
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub user: Signer<'info>, // the user, and signer
 
-    pub mint: Account<'info, Mint>,
+    pub mint: Account<'info, Mint>, // the mint to be staked
 
-    pub collection_mint: Account<'info, Mint>,
+    pub collection_mint: Account<'info, Mint>, // the collection to which the mint belongs to
 
     #[account(
         mut,
         associated_token::mint = mint,
         associated_token::authority = user,
     )]
-    pub user_mint_ata: Account<'info, TokenAccount>,
+    pub user_mint_ata: Account<'info, TokenAccount>, // user's mint ata
 
     /*
     In the Metaplex Token Metadata program, 
@@ -36,17 +51,18 @@ pub struct Stake<'info> {
     in your #[derive(Accounts)] by specifying the exact same seeds.
      */
     #[account(
-        seeds = [b"metadata", metadata_program.key().as_ref(), mint.key().as_ref()],
-        seeds::program = metadata_program.key(),
+        seeds = [b"metadata", metadata_program.key().as_ref(), mint.key().as_ref()], 
+        seeds::program = metadata_program.key(), // basically, these two steps let us to obtain & mirror the metadata of the mint from Metaplex, for confirmation
         bump,
-        constraint = metadata.collection.as_ref().unwrap().key.as_ref() ==
-        collection_mint.key().as_ref(),
+        constraint = metadata.collection.as_ref().unwrap().key.as_ref() == collection_mint.key().as_ref(),
         constraint = metadata.collection.as_ref().unwrap().verified == true
     )]
-    pub metadata: Account<'info, MetadataAccount>,
+    pub metadata: Account<'info, MetadataAccount>, 
+    // constraints are security checks, transaction will fail is these checks are not met
 
     /*
-    	Confirms this is a “v1” NFT (MasterEdition) so freezing semantics apply correctly
+    	Confirms this is a “v1” NFT (MasterEdition) so freezing semantics apply correctly.
+        Therefore, this ensures that if the given account is not a master edition, the transaction fails immediately
      */
     #[account(
         seeds = [b"metadata", metadata_program.key().as_ref(), mint.key().as_ref(), b"edition"],
@@ -55,7 +71,10 @@ pub struct Stake<'info> {
     )]
     pub edition: Account<'info, MasterEditionAccount>,
 
-    #[account(seeds = [b"config"], bump = config.bump)]
+    #[account(
+        seeds = [b"config"], 
+        bump = config.bump
+    )]
     pub config: Account<'info, StakeConfig>,
 
     #[account(
@@ -73,24 +92,31 @@ pub struct Stake<'info> {
         bump
     )]
     pub stake_account: Account<'info, StakeAccount>,
+
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
     pub metadata_program: Program<'info, Metadata>,
 }
-
+/*
+    Steps: 
+    (1) get approval 
+    (2) freeze the tokens in the user wallet
+    (3) create the stake account for the mint
+    (4) increase user staked amount
+*/
 impl<'info> Stake<'info> {
     pub fn stake(&mut self, bumps: &StakeBumps) -> Result<()> {
-        require!(
-            self.user_account.amount_staked < self.config.max_stake,
-            StakeError::MaxStakeReached
-        );
+        
+        require!( self.user_account.amount_staked < self.config.max_stake, StakeError::MaxStakeReached);
 
         let cpi_program = self.token_program.to_account_info();
 
         /*
-            what approve is doing - 
-            Let stake_account spend 1 token from user_mint_ata, which is owned by user.
-         */
+            Function of Approve:
+            The program makes a CPI to the SPL Token Program. This approve call doesn't move the NFT. 
+            Instead, the user (as the authority) gives the stake_account PDA permission (delegates authority) to manage the token in their user_mint_ata. 
+            This permission is what allows the stake_account to freeze the NFT in the next step.
+        */
         let cpi_accounts = Approve {
             to: self.user_mint_ata.to_account_info(), // token account owned by the user
             delegate: self.stake_account.to_account_info(), //stake account will be the delegate
